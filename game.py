4d91f2f7a43b8e93ee61dfd05622ef8b9d3b5e98
ashu-py <88982064+ashu-py@users.noreply.github.com>
2021-09-25 14:27:28 +0530

    Update game.py

--- game.py
+++ game.py
@@ -3,30 +3,43 @@ from termcolor import colored
 print(colored('for stone entre = > 1 ','green'))	
 print(colored('for paper entre = > 2 ','green'))
 print(colored('for scissor entre = > 3 ','green'))		
+sc=0
 try:
 	while True:
 		a=r.randint(1,3)
-		#print(a)
 		
+		
+		print('[^0^] score=>',sc)
 		print(colored('entre => ','green'))
 		q =int(input())
 		if a==1:
 			if q==2:
 				print(colored('YOU WON !!!! ','green'))
+				sc=sc+1
+				
 			if q==3:
 				print(colored('####HELLO MR.LOSER####','red'))
+				
 		if a==2:
 			if q==3:
 				print(colored('YOU WON !!!! ','green'))
+				sc=sc+1
+				
 			if q==1:
 				print(colored('####HELLO MR.LOSER####','red'))
+				
 		if a==3:
 			if q==1:
 				print(colored('YOU WON !!!! ','green'))
+				sc=sc+1
+				
 			if q==2:
 				print(colored('####HELLO MR.LOSER####','red'))
+				
 		if a==q:
 				print(colored('@ ITS DORE ','blue'))
+		#print('[^0^] score=>',sc)
 except:
 			pass
+			
 			
